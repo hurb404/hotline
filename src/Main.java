24c21a18284b15f467254db31ff42632a8197fd1
Saket <bansals@asbindia.org>
2022-09-02 19:10:14 +0530

    added conditional for special characters to add 0 to the end of the string

--- src/Main.java
+++ src/Main.java
@@ -64,6 +64,8 @@ public class Main {
                 final_number_temp = final_number_temp + "9";
             } else if (ch[i] == '0' || ch[i] == '1' || ch[i] == '2' || ch[i] == '3' || ch[i] == '4' || ch[i] == '5' || ch[i] == '6' || ch[i] == '7' || ch[i] == '8' || ch[i] == '9') {
                 final_number_temp = final_number_temp + ch[i];
+            } else {
+                final_number_temp = final_number_temp + "0";
             }
         }
 
